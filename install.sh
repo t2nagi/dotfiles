4b23cbdac130f111951359011df2fdaa9fe4b510
t.yanagita <t.yanagita.se@gmail.com>
2021-01-03 02:27:33 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Insert Homebrew & Git
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install git
-git clone https://github.com/t2nagi/dotfiles.git
-
 # Insert dotfiles
+git clone https://github.com/t2nagi/dotfiles.git
 cd dotfiles
 ROOT_DIR=$(pwd)
 for dotfile in `find $ROOT_DIR/home -type f`; do
